4684170d190aa44bc7fc4d930a6eba8911a2e178
Paraross <Adrago@onet.pl>
2024-04-18 22:26:20 +0200

    Clean main function

--- main.cpp
+++ main.cpp
@@ -1,12 +1,7 @@
 #include "estimate_pi_async.h"
 #include "readonly/bench.h"
 
-const uint32_t POINT_COUNT = (uint32_t)1e+6;
-const uint32_t THREAD_COUNT = 4;
-
 int main()
 {
-    bench_sync(POINT_COUNT);
-    bench_async<THREAD_COUNT>(POINT_COUNT);
-    bench_async_vec(POINT_COUNT, THREAD_COUNT);
+
 }
